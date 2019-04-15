393de9c07729c18b2283486bc1611e7582084caa
tonkori5 <tonkatsusokuhou@gmail.com>
2019-04-16 01:20:21 +0900

    test

--- venv/myapp.py
+++ venv/myapp.py
@@ -1,15 +1,3 @@
-x = 10
-print(x / 3)
-print(x // 3)
-print(x ** 2)
+print('Yes, they said.')
 
-y = 4
-y += 12
-print(y)
-
-print(True and False)
-print(True or False)
-print(not True)
-
-print("hello " + "world")
-print("hello \n" * 3)
+print('"Yes," they said.')
